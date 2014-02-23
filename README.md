cf9bc7e556fa83fb8b800a5b34ca6d4eff503b69
Dylan O'Shea <dylan.j.oshea@gmail.com>
2014-02-22 22:15:24 -0800

    Removing RailJumper Readme, making times slightly more accurate

--- README.md
+++ README.md
@@ -1,46 +1,6 @@
 # ScreenGuide
 
-Setting up Rails apps can get tedious -- repeating the same steps and downloading the same packages. This pre-packaged repository aims to reduce setup time and aid in getting off the ground quickly.
-
-### Features
-  * User model
-  * Session-based log-in
-  * Site admin
-  * Simple navbar
-  * Foundation 5-ready
-
-### Setup
-1. Clone the repo down: <tt>git clone https://github.com/doshea/screen_guide.git</tt>
-2. Rename the cloned directory: <tt>mv screen_guide your_app_name</tt>
-3. Delete the .git folder inside the repo: <tt>rm -rf .git</tt>
-4. Use Replace All (Command + Shift + F in Sublime) within your project to replace the following case-sensitive placeholders with the appropriate names for your application:
-
-  | Placeholder         |          |           Replacement         |
-  | :------------: |:-----:|:--------------------:|
-  | screen_guide         |   --> |        your_app_name       |
-  | ScreenGuide          |   --> |         YourAppName        |
-  | your_name_here |   --> | your_postgres_username |
-
-5. Rename screen_guide.js and .css.scss to your_app_name.js and .css.scss
-6. Create and migrate the database: <tt>rake db:create db:migrate</tt>
-7. Party down!
-
-### Frameworks/Libraries
-ScreenGuide comes bundled with a number of libraries/frameworks to minimize the time you spend downloading and setting up.
-
-#### HTML/CSS/JS Framework
-  * [Foundation 5](http://foundation.zurb.com/docs/)
-  * [Foundation Icons 3](http://zurb.com/playground/foundation-icon-fonts-3)
-
-#### Javascript Libraries (DISABLED by default)
-  * [Underscore.js](http://underscorejs.org/)
-  * [Sugar.js](http://sugarjs.com/)
-  * [Moment.js](http://momentjs.com/)
-  * [Morris.js](http://momentjs.com/)
-  * [jQuery UI](https://jqueryui.com/)
-  * [d3](http://d3js.org/)
-  * [Chart.js](http://www.chartjs.org/)
-  * [DataTables](https://datatables.net/)
+A simple application that allows users to track which TV shows they are watching and particularly which episodes have recently aired.
 
 =====
 This README written in [Github Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
--- app/controllers/admin/shows_controller.rb
+++ app/controllers/admin/shows_controller.rb
@@ -5,45 +5,71 @@ class Admin::ShowsController < ApplicationController
     @show = Show.new
   end
 
-  def create 
+  def create
     @show = Show.new(admin_show_params)
     if @show.save
       redirect_to @show
     else
       render :new
     end
-    
+
   end
 
   def rage_create
-    new_show_data = HTTParty.get("http://services.tvrage.com/feeds/episode_list.php?sid=#{params[:rage_id]}")['Show']
+    new_show_data = HTTParty.get("http://services.tvrage.com/feeds/episode_list.php?sid=#{params[:rage_id]}")
+    symbolize_and_downcase_keys_deep!(new_show_data)
+    new_show_data = new_show_data[:show]
     new_show = Show.create(
-      name: new_show_data['name'],
+      name: new_show_data[:name],
       rage_id: params[:rage_id]
     )
-    total_seasons = new_show_data['totalseasons'].to_i
+    total_seasons = new_show_data[:totalseasons].to_i
 
     (1..total_seasons).each do |i|
       new_season = Season.create(number: i)
       new_show.seasons << new_season
       if total_seasons > 1
-        temp_episodes = new_show_data['Episodelist']['Season'][i-1]['episode']
+        temp_episodes = new_show_data[:episodelist][:season][i-1][:episode]
       else
-        temp_episodes = new_show_data['Episodelist']['Season']['episode']
+        temp_episodes = new_show_data[:episodelist][:season][:episode]
       end
       if temp_episodes.class != Array
         temp_episodes = [temp_episodes]
       end
       temp_episodes.each do |temp_episode|
         new_episode = Episode.create(
-          number: temp_episode['seasonnum'].to_i,
-          air_date: Date.parse(temp_episode['airdate']),
-          title: temp_episode['title']
+          number: temp_episode[:seasonnum].to_i,
+          air_date: Date.parse(temp_episode[:airdate]),
+          title: temp_episode[:title]
         )
         new_season.episodes << new_episode
       end
     end
     redirect_to new_show
+  end
+
+  def check_for_new_episodes
+    show = Show.find(params[:id])
+    rage_data = HTTParty.get("http://services.tvrage.com/feeds/episode_list.php?sid=#{show.rage_id}")
+    symbolize_and_downcase_keys_deep!(rage_data)
+    show_data = rage_data[:show]
+
+    total_seasons = show_data[:totalseasons].to_i
+    existing_seasons = show.seasons
+
+    seasons_added = false
+    episodes_added = false
+
+    if total_seasons > existing_seasons.count
+      (existing_seasons.count+1..total_seasons).each do |i|
+        show.seasons << Season.create(number: i)
+        seasons_added = (seasons_added or 0) +1
+      end
+    end
+
+
+
+
   end
 
   def edit
@@ -70,6 +96,22 @@ class Admin::ShowsController < ApplicationController
   end
   def rage_show_params
     params.require(:show).permit(:name, :rage_id)
+  end
+
+  def symbolize_and_downcase_keys_deep!(h)
+    puts "#{h.class} -- #{h}" 
+    if h.kind_of? Hash
+      h.keys.each do |k|
+        ks    = k.respond_to?(:to_sym) ? k.downcase.to_sym : k
+        h[ks] = h.delete k # Preserve order even when k == ks
+        symbolize_and_downcase_keys_deep! h[ks] if ((h[ks].kind_of? Hash) or (h[ks].kind_of? Array))
+      end
+    elsif h.kind_of? Array
+      puts 'Array!'
+      h.each do |el|
+        symbolize_and_downcase_keys_deep! el if (el.kind_of? Hash or el.kind_of? Array)
+      end
+    end
   end
 
 end

3f5de2c5d30884afc648a4370338ae31fbd61a64
Dylan O'Shea <dylan.j.oshea@gmail.com>
2015-09-02 00:59:54 -0700

    Adding stuff

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,9 @@
 class AdminController < ApplicationController
+  before_action :ensure_admin
+
+  def clone_user
+    user = User.find params[:id]
+    cookies[:auth_token] = user.auth_token
+    redirect_to root_path
+  end
 end
